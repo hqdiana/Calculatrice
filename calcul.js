02228140ffe154d28191fce780fc87a6d943289c
Diana Qureshi <259169993+hqdiana@users.noreply.github.com>
2026-02-19 14:00:30 -0500

    add comments for script algorithm

---
+++ calcul.js
@@ -0,0 +1,8 @@
+// create empty variable for number being clicked
+// create variable for previous number clicked
+// create variable for selected operator
+// create function to display operation
+    // take current input and push it into the DOM
+// create function to operate and display result
+    // convert stringed numbers to actual numbers
+    // perform calculation and return result
